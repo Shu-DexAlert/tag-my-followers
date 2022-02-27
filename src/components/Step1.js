d233462f95778cba4e42795c7170f5fc0bd14a3b
Patrick Pu <pxlxinzhao@gmail.com>
2022-02-27 16:59:00 -0500

    add enter

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -13,7 +13,7 @@ export default function Step1(props) {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">What's your twitter handle?</h5>
 
             <div className="flex items-center justify-center">
-                <input ref={inputEl} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Type handle without @" />
+                <input ref={inputEl} onKeyDown={e => e.key === 'Enter' && onConnect()} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Type handle without @" />
                 <button type="button" className="ml-3 mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={onConnect}>Connect</button>
             </div>
 
